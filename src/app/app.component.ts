0e754a39e5873d2a021c7278bc83e694bbc59b29
tangaye <siafanathan@gmail.com>
2024-02-19 15:34:55 +0000

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'pitch-demo';
+	title = 'pitch-demo';
+
+	file: File | null = null;
+
+	@ViewChild('fileInput') fileInput: any;
+
+	openFilePicker() {
+		this.fileInput.nativeElement.click();
+	}
+
+	fileSelected() {
+		if (this.fileInput.nativeElement.files.length) {
+			this.file = this.fileInput.nativeElement.files[0];
+			return;
+		}
+
+		this.file = null;
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		MatButtonModule,
+		MatIconModule,
+		MatFormFieldModule,
+		MatInputModule,
+		FormsModule,
+	],
+	providers: [provideAnimationsAsync()],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
